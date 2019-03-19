64d97e4f39416876d3aca7becc0fb7fc064af6cf
gaga201702 <bamboo2017002@gmail.com>
2019-03-19 22:13:44 +0700

    order

---
+++ src/components/order/PageOrder.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import Intro from "./Intro";
+import ContactForm from "./ContactForm";
+
+export default class PageOrder extends Component{
+  render() {
+    return (
+      <div>
+        <Intro/>
+        <ContactForm/>
+      </div>
+    );
+  }
+
+}
