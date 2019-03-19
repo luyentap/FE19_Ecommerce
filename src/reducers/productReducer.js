0732c123c5b64f3dc648ea04ea87c8565063831b
gaga201702 <bamboo2017002@gmail.com>
2019-03-19 20:22:57 +0700

    cart

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,4 +1,4 @@
-import {ADD_TO_CART, PRODUCT_FETCH_SUCCESS} from "../actions/ActionTypes";
+import {ADD_QUANTITY, ADD_TO_CART, PRODUCT_FETCH_SUCCESS, REMOVE_ITEM, SUB_QUANTITY} from "../actions/ActionTypes";
 import {combineReducers} from 'redux'
 import ProductService from "../service/ProductService";
 import React from "react";
@@ -40,6 +40,50 @@ export const productReducer = (state=initSate, action) =>{
           ...state,
           addedItems: [...state.addedItems, addedItem],
           total : newTotal
+        }
+      }
+    case REMOVE_ITEM:
+      //calculating the total
+      var itemToRemove= state.addedItems.find(item=> action.id == item.id);
+      let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity );
+      console.log(itemToRemove);
+
+      var new_items = state.addedItems.filter(item=> action.id != item.id);
+      return{
+        ...state,
+        addedItems: new_items,
+        total: newTotal
+      }
+    case ADD_QUANTITY:
+      var addedItem = state.addedItems.find(item=> item.id === action.id);
+      addedItem.quantity += 1;
+
+      var newTotal = state.total + addedItem.new_price;
+      console.log("total",state.total);
+      return{
+        ...state,
+        addedItems: [...state.addedItems],
+        total : newTotal
+      }
+    case SUB_QUANTITY:
+      var addedItem = state.addedItems.find(item=> item.id === action.id);
+      //Nếu số lượng bằng 0, gỡ sản phẩm ra khỏi list
+      if(addedItem.quantity === 1){
+        let new_items = state.addedItems.filter(item=>item.id !== action.id)
+        let newTotal = state.total - addedItem.new_price
+        return{
+          ...state,
+          addedItems: new_items,
+          total: newTotal
+        }
+      }
+      else {
+        addedItem.quantity -= 1
+        let newTotal = state.total - addedItem.new_price
+        return{
+          ...state,
+          addedItems: [...state.addedItems],
+          total: newTotal
         }
       }
     default: // need this for default case
