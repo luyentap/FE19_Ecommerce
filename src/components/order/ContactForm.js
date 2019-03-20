bf55ec6b0385638483feb224aca7d6f7800da35e
gaga201702 <bamboo2017002@gmail.com>
2019-03-20 09:13:11 +0700

    order

--- src/components/order/ContactForm.js
+++ src/components/order/ContactForm.js
@@ -1,32 +1,47 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {loadState} from "../../common/LocalSave";
+import OrderService from "../../service/OrderService";
 
-export default class ContactForm extends Component{
+export default class ContactForm extends Component {
+  orderProduct = (e) => {
+    e.preventDefault();
+    //lấy giá trị input thông tin người dùng
+    let name = this.getName.value;
+    let phone = this.getPhoneNumber.value;
+    let address = this.getAddress.value;
+
+    //lấy giá trị(cart+total) trong localstorage -->init reducer
+    let addedItems = loadState("shoppingCart") || [];
+
+    let data = {"cart": addedItems, personal: {name, phone, address}};
+    OrderService.postOrder(data);
+    // this.getName.value = "";
+  }
+
   render() {
     return (
-      <div className="container">
         <div className="register">
-          <form className="form">
+          <form className="form" onSubmit={this.orderProduct}>
             <h3 className="form__title">THÔNG TIN CÁ NHÂN</h3>
             <div className="form-element">
               <label>Họ và Tên<span className="upper-text">*</span></label>
-              <input className="form-element__name" />
+              <input className="form-element__name" ref={(input) => this.getName = input}/>
             </div>
             <div className="form-element">
               <label>Địa chỉ<span className="upper-text">*</span></label>
-              <input className="form-element__address"/>
+              <input className="form-element__address" ref={(input) => this.getAddress = input}/>
             </div>
             <div className="form-element">
               <label>Điện thoại<span className="upper-text">*</span></label>
-              <input className="form-element__phone"/></div>
+              <input className="form-element__phone" ref={(input) => this.getPhoneNumber = input}/>
+            </div>
             <div className="form-element form-element__button">
-              <button onClick="location.href='payment-sucess.html'" type="button">Gửi</button>
-              <button onClick="location.href='shopping-cart.html'" type="button"><i className="fa fa-undo"></i>Quay lại
-              </button>
+              <button type="submit">Gửi</button>
+              <Link to="/cart" >Quay về</Link>
             </div>
           </form>
         </div>
-      </div>
     );
   }
 
--- src/components/order/OrderSucess.js
+++ src/components/order/OrderSucess.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 
 export default class OrderSucess extends Component{
+
   render() {
     return (
       <div>
